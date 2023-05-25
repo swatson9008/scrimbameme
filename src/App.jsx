8edd0fca514eb50bd6c249c1c4f6414dc2edaf14
Summer <swatson9008@yahoo.com>
2023-05-25 15:06:38 -0400

    created the meme component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header'
+import Meme from './components/meme'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Meme />
     </>
   )
 }
---
+++ src/components/meme.jsx
@@ -0,0 +1,11 @@
+export default function Meme(){
+    return(
+        <div className="Meme">
+            <form className="MemeForm">
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="submit" value="Get a new meme image 🖼️" className="SubmitButton"></input>
+            </form>
+        </div>
+    )
+}
