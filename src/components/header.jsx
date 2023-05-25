00e3184c06641210d2c7c3d5586ffbc6c25183fc
Summer <swatson9008@yahoo.com>
2023-05-25 13:10:33 -0400

    boilerplate

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div className="Header">
+            <span className="Title">Meme Generator</span>
+            <span className="Course">React Course - Project 3</span>
+        </div>
+    )
+}
