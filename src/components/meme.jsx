d02e500a2acd9be890a577b7b6cdc1db3e6e309e
Summer <swatson9008@yahoo.com>
2023-05-26 14:21:59 -0400

    fixes

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,10 +1,19 @@
+import memesData from "../assets/memesData"
+
 export default function Meme(){
+    function memeDisplay(){
+        event.preventDefault();
+        const memesArray = memesData.data.memes;
+        const genNumber = Math.floor(Math.random() * memesArray.length);
+        console.log(memesArray[genNumber].url);
+
+    }
     return(
         <div className="Meme">
             <form className="MemeForm">
-                <input type="text"></input>
-                <input type="text"></input>
-                <input type="submit" value="Get a new meme image 🖼️" className="SubmitButton"></input>
+                <input type="text" placeholder="Top text"></input>
+                <input type="text" placeholder="Bottom text"></input>
+                <input type="submit" value="Get a new meme image 🖼️" className="SubmitButton" onClick={memeDisplay}></input>
             </form>
         </div>
     )
