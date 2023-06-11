6ed35f84af86b4bc8f5995daa133684b38a4cc63
Summer <swatson9008@yahoo.com>
2023-06-11 19:34:14 -0400

    meme input works

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -4,12 +4,21 @@ import memesData from "../assets/memesData"
 export default function Meme(){
 
     const [meme, imageShift] = useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "",
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        randomImage: "http://i.imgflip.com/1bij.jpg",
     });
 
     const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+    function handleChange(event){
+        imageShift(prevMeme => {
+            return {
+                ...prevMeme,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
 
     function memeDisplay(){
         event.preventDefault();
@@ -25,11 +34,15 @@ export default function Meme(){
     return(
         <div className="Meme">
             <form className="MemeForm">
-                <input type="text" placeholder="Top text"></input>
-                <input type="text" placeholder="Bottom text"></input>
+                <input type="text" name="topText" onChange={handleChange} placeholder="Top text"></input>
+                <input type="text" name="bottomText" onChange={handleChange} placeholder="Bottom text"></input>
                 <input type="submit" value="Get a new meme image 🖼️" className="SubmitButton" onClick={memeDisplay}></input>
             </form>
+            <div className="memeEntry">
             <img src={meme.randomImage} className="memeImages"/>
+            <h2 className="meme--text top">{meme.topText}</h2>
+            <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
